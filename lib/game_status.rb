c4978d63492aa97812b06da7aba80b3a4b504ab2
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-22 04:56:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,14 +25,14 @@ def won?(board)
       position_2 = board[win_index_2]
       position_3 = board[win_index_3]
       if position_1 == "X" && position_2 == "X" && position_3 == "X"
-        new_arr.push(array[0])
-        new_arr.push(array[1])
-        new_arr.push(array[2])
+        new_arr.push(WIN_COMBINATIONS[0])
+        new_arr.push(WIN_COMBINATIONS[1])
+        new_arr.push(WIN_COMBINATIONS[2])
         return new_arr
       elsif position_1 == "O" && position_2 == "0" && position_3 == "0"
-        new_arr.push(array[0])
-        new_arr.push(array[1])
-        new_arr.push(array[2])
+        new_arr.push(WIN_COMBINATIONS[0])
+        new_arr.push(WIN_COMBINATIONS[1])
+        new_arr.push(WIN_COMBINATIONS[2])
         return new_arr
       end
     end
